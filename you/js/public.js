5595b85a586aab7562049b59b49c2556f9c105cf
maxapply <291325561@qq.com>
2020-07-05 23:45:29 +0800

    首页

---
+++ you/js/public.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.banner1', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
